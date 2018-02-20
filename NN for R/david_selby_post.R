348a4d73838b802551e40ddb96e9ae5fd3ba9818
byram <byram0@gmail.com>
2018-02-19 20:58:35 -0500

    Formatting
    Cleaned up comma spacing a bit in the R6 chunk

--- NN for R/david_selby_post.R
+++ NN for R/david_selby_post.R
@@ -107,11 +107,11 @@ ggplot(hotdogs, aes(x1, x2, col = class)) +
   labs(x = expression(x[1]), y = expression(x[2]))
 
 library(R6)
-NeuralNetwork <- R6Class("NeuralNetwork",
+NeuralNetwork <- R6Class("NeuralNetwork", 
                          public = list(
-                           X = NULL,  Y = NULL,
-                           W1 = NULL, W2 = NULL,
-                           output = NULL,
+                           X = NULL,  Y = NULL, 
+                           W1 = NULL, W2 = NULL, 
+                           output = NULL, 
                            initialize = function(formula, hidden, data = list()) {
                              # Model and training data
                              mod <- model.frame(formula, data = data)
@@ -124,16 +124,16 @@ NeuralNetwork <- R6Class("NeuralNetwork",
                              # Initial weights and bias
                              self$W1 <- .01 * matrix(rnorm(D * H), D, H)
                              self$W2 <- .01 * matrix(rnorm((H + 1) * K), H + 1, K)
-                           },
+                           }, 
                            fit = function(data = self$X) {
                              h <- self$sigmoid(data %*% self$W1)
                              score <- cbind(1, h) %*% self$W2
                              return(self$softmax(score))
-                           },
+                           }, 
                            feedforward = function(data = self$X) {
                              self$output <- self$fit(data)
                              invisible(self)
-                           },
+                           }, 
                            backpropagate = function(lr = 1e-2) {
                              h <- self$sigmoid(self$X %*% self$W1)
                              Yid <- match(self$Y, sort(unique(self$Y)))
@@ -144,37 +144,37 @@ NeuralNetwork <- R6Class("NeuralNetwork",
                              self$W1 <- self$W1 - lr * dW1
                              self$W2 <- self$W2 - lr * dW2
                              invisible(self)
-                           },
+                           }, 
                            predict = function(data = self$X) {
                              probs <- self$fit(data)
                              preds <- apply(probs, 1, which.max)
                              levels(self$Y)[preds]
-                           },
+                           }, 
                            compute_loss = function(probs = self$output) {
                              Yid <- match(self$Y, sort(unique(self$Y)))
                              correct_logprobs <- -log(probs[cbind(seq_along(Yid), Yid)])
                              sum(correct_logprobs)
-                           },
-                           train = function(iterations = 1e4,
-                                            learn_rate = 1e-2,
-                                            tolerance = .01,
+                           }, 
+                           train = function(iterations = 1e4, 
+                                            learn_rate = 1e-2, 
+                                            tolerance = .01, 
                                             trace = 100) {
                              for (i in seq_len(iterations)) {
                                self$feedforward()$backpropagate(learn_rate)
                                if (trace > 0 && i %% trace == 0)
-                                 message('Iteration ', i, '\tLoss ', self$compute_loss(),
+                                 message('Iteration ', i, '\tLoss ', self$compute_loss(), 
                                          '\tAccuracy ', self$accuracy())
                                if (self$compute_loss() < tolerance) break
                              }
                              invisible(self)
-                           },
+                           }, 
                            accuracy = function() {
                              predictions <- apply(self$output, 1, which.max)
                              predictions <- levels(self$Y)[predictions]
                              mean(predictions == self$Y)
-                           },
-                           sigmoid = function(x) 1 / (1 + exp(-x)),
-                           dsigmoid = function(x) x * (1 - x),
+                           }, 
+                           sigmoid = function(x) 1 / (1 + exp(-x)), 
+                           dsigmoid = function(x) x * (1 - x), 
                            softmax = function(x) exp(x) / rowSums(exp(x))
                          )
 )
