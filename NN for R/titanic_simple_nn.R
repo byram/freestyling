737d5ee6ea466d364c6ccc8322d2163fc78171c4
byram <byram0@gmail.com>
2018-03-05 18:44:23 -0500

    Titanic NN
    Finally got a simple densely connected NN going for the Titanic data set.
     ~ 77.5% accuracy
     ~ 0.50 loss

--- NN for R/titanic_simple_nn.R
+++ NN for R/titanic_simple_nn.R
@@ -1,25 +1,51 @@
-require(keras)
-require(dplyr)
-require(tidyr)
-require(data.table)
+library(keras)
+library(dplyr)
+library(tidyr)
+library(data.table)
 set.seed(42)
 
-batch_size <- 128
-num_classes <- 2
-epochs <- 10
+#batch_size <- 128
+#num_classes <- 2
+#epochs <- 10
 
-titanicData <- Titanic %>%
-  data.table() %>%
-  mutate(N = as.integer(N)) %>%
-  {.[rep(1:nrow(.), .[, 5]), -5]} %>%
+titanicData <- Titanic %>% 
+  data.table(stringsAsFactors = T) %>% 
+  mutate(N = as.integer(N)) %>% 
+  {.[rep(1:nrow(.), .[, 5]), -5]} %>% 
   {.[sample(1:nrow(.)), ]}
 
+titanicDataBin <- titanicData %>% 
+  {cbind(to_categorical(.$Class)[, -1], 
+         to_categorical(.$Sex)[, -1], 
+         to_categorical(.$Age)[, -1], 
+         to_categorical(.$Survived)[, -1])}
+
 # Training dataset
-xTrain <- titanicData[1:round(nrow(titanicData) * 0.8, 0), -4]
+partitionTrain <- titanicDataBin[1:round(nrow(titanicDataBin) * 0.8, 0), ]
+xTrain <- partitionTrain[, -c(9, 10)]
+yTrain <- partitionTrain[, c(9:10)]
+# Testing dataset
+xTest <- titanicDataBin[-c(1:nrow(partitionTrain)), -c(9, 10)]
+yTest <- titanicDataBin[-c(1:nrow(partitionTrain)), c(9, 10)]
 
-yTrain <- titanicData[1:nrow(xTrain), 4] %>%
-  mutate()
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 128, activation = "relu", input_shape = c(8)) %>% 
+  layer_dropout(rate = 0.2) %>% 
+  layer_dense(units = 128, activation = "relu") %>% 
+  layer_dropout(rate = 0.2) %>% 
+  layer_dense(units = 2, activation = "softmax")
 
+model %>% compile(
+  loss = "categorical_crossentropy", 
+  optimizer = optimizer_adam(), 
+  metrics = c("accuracy")
+)
 
-xTrain <- titanicData[1:round(nrow(titanicData), 0), -4]
-yTrain <- titanicData[1:round(nrow(titanicData), 0), 4]
+history <- model %>% fit(
+  xTrain, yTrain, 
+  epochs = 30, batch_size = 128, 
+  validation_split = 0.2
+)
+
+model %>% evaluate(xTest, yTest)
